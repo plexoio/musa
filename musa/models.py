68296f726a5ae45c71d951d697acf79752d3a41f
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-07-24 14:39:15 +0000

    feat: complete models.py ORMs, moving forward

--- musa/models.py
+++ musa/models.py
@@ -2,7 +2,45 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, 'Draft')), (1, 'Published')
+STATUS = (
+    (0, 'Draft'),
+    (1, 'Published'),
+    (3, 'Disapproved')
+)
+
+ROLES = (
+    (0, 'User'),
+    (1, 'Publisher'),
+)
+
+
+# Category Model
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['category_name']
+
+    def __str__(self):
+        return self.category_name
+
+# User Extended
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    verified = models.BooleanField(default=False)
+    role = models.IntegerField(choices=ROLES, default=0)
+    vote_records = models.ManyToManyField(
+        'VoteCard', through='VoteRecord', related_name='vote_records')
+
+    class Meta:
+        ordering = ['user__username']
+
+    def __str__(self):
+        return self.user.username
 
 # VoteCard Model
 
@@ -11,16 +49,16 @@ class VoteCard(models.Model):
     title = models.CharField(max_length=80, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="vote_cards")
+        UserProfile, on_delete=models.CASCADE, related_name="vote_cards")
     category = models.ForeignKey(
-        category, on_delete=models.CASCADE, related_name="categories")
+        Category, on_delete=models.CASCADE, related_name="categories")
     mission = models.CharField(max_length=80)
     location = models.CharField(max_length=80)
     description = models.TextField(max_length=400)
-    expire = models.DateField(auto_now=True)
+    expire = models.DateField(auto_now_add=True)
     event_image = CloudinaryField('image', default='placeholder')
     vote_record = models.ManyToManyField(
-        User, through='VoteRecord', related_name='user_votes', blank=True)
+        UserProfile, through='VoteRecord', related_name='user_votes', blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     created_on = models.DateField(auto_now_add=True)
@@ -28,8 +66,39 @@ class VoteCard(models.Model):
     class Meta:
         ordering = ['-created_on']
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
 
-        def number_of_votes(self):
-            return self.vote_record.count()
+    def number_of_votes(self):
+        return self.vote_record.count()
+
+# VoteRecord model
+
+
+class VoteRecord(models.Model):
+    voter = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
+    vote_card = models.ForeignKey(
+        VoteCard, on_delete=models.PROTECT, related_name="votecard_record")
+    elected_person = models.ForeignKey(
+        'ElectedPerson', on_delete=models.PROTECT, related_name="elected_record")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['timestamp']
+
+    def __str__(self):
+        return self.elected_person.name
+
+
+# ElectedPerson
+
+
+class ElectedPerson(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    is_elected = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
