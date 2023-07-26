37200d2501e8b1a258a514ebd10ac2d7bdedb094
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-07-26 00:47:28 +0000

    refactor: add context object name on views.py & READ for testing on frontend

--- musa/urls.py
+++ musa/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    # Backend
     path('user/', views.UserDashboard.as_view(
     ), name="user_dashboard"),
     path('user/settings/', views.UserSetting.as_view(
@@ -9,5 +10,9 @@ urlpatterns = [
     path('office/', views.AdminDashboard.as_view(
     ), name="admin_dashboard"),
     path('office/settings/', views.AdminSetting.as_view(
-    ), name="admin_settings")
+    ), name="admin_settings"),
+
+    # Frontend
+    path('', views.HomePage.as_view(
+    ), name="homepage")
 ]
--- musa/views.py
+++ musa/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from django.views import generic
 from .models import VoteCard, UserProfile
 
+# Backend Controllers
 
-class UserDashboard(generic.ListView):
+
+class BaseListView(generic.ListView):
     model = VoteCard
-    queryset = VoteCard.objects.filter(status=1).order_by('-created_on')
-    template_name = 'backend/user-dashboard/index.html'
     paginate_by = 6
+
+    def get_queryset(self):
+        return VoteCard.objects.filter(status=1).order_by('-created_on')
+
+
+class UserDashboard(BaseListView):
+    template_name = 'backend/user-dashboard/index.html'
 
 
 class UserSetting(generic.ListView):
@@ -15,13 +22,17 @@ class UserSetting(generic.ListView):
     template_name = 'backend/user-dashboard/settings.html'
 
 
-class AdminDashboard(generic.ListView):
-    model = VoteCard
-    queryset = VoteCard.objects.filter(status=1).order_by('-created_on')
+class AdminDashboard(BaseListView):
     template_name = 'backend/admin-dashboard/index.html'
-    paginate_by = 6
 
 
 class AdminSetting(generic.ListView):
     model = UserProfile
     template_name = 'backend/admin-dashboard/settings.html'
+
+
+# Frontend Controllers
+
+class HomePage(BaseListView):
+    template_name = 'frontend/index.html'
+    context_object_name = 'home_page'
