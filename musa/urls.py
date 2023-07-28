a724aa09e2f894b2c49589002da839752a886169
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-07-28 13:58:47 +0000

    refactor: urls.py comments add

--- musa/urls.py
+++ musa/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views, role_redirect
 
 urlpatterns = [
-    # Backend
+    # BACKEND
+
     path('login/', views.CustomLoginView.as_view(),
          name='account_login'),
     path('signup/', views.CustomSignupView.as_view(), name='account_signup'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    # USER
     path('user/', views.UserDashboard.as_view(),
          name='user_dashboard'),
     path('user/settings/', views.UserSettings.as_view(),
@@ -14,16 +16,18 @@ urlpatterns = [
     path('user/password-change/', views.UserPasswordChangeView.as_view(),
          name='user_change'),
     path('user/delete/', views.UserDelete.as_view(), name='user_delete'),
+    # OFFICE
     path('office/', views.AdminDashboard.as_view(),
          name='admin_dashboard'),
     path('office/settings/', views.AdminSettings.as_view(),
          name='admin_settings'),
     path('office/password-change/', views.AdminPasswordChangeView.as_view(),
          name='admin_change'),
+    # ROLE
     path('role_redirect/', role_redirect.RoleRedirectView.as_view(),
          name='role_redirect'),
 
-    # Frontend
+    # FRONTEND
     path('', views.HomePage.as_view(),
          name='homepage'),
     path('vote/<int:vote_card_id>/', views.VoteForCardView.as_view(),
