831f4ffa1dd668f94fbe6748fae7bc718d6fe26d
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-08-06 11:49:45 +0000

    faat: contact form complete with feedback using js and bootstrap

--- static/custom/js/contact_form.js
+++ static/custom/js/contact_form.js
@@ -8,8 +8,21 @@ function sendMail(contactForm, event) { // as 'this' on the form tag
     }).then(
         function (response) {
             console.log('Success', response);
-        }, function (error) {
+            $('#email_sent').html(`
+            <div class="alert alert-success alert-dismissible text-center" role="alert" tabindex="-1">
+                <div>Your email has been sent successfully we'll be in touch with you shortly!</div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>`);
+            $('#email_sent .alert').focus()
+        },
+        function (error) {
             console.log('Failed', error);
+            $('#email_sent').html(`
+            <div class="alert alert-danger alert-dismissible text-center" role="alert" tabindex="-1">
+           <div>STATUS: ${error.status} and ERROR: ${error.text}</div>
+           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+           </div>`);
+            $('#email_sent .alert').focus()
         }
     );
 };
