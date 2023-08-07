a32e76bac98bd65b1eaac1cc0e7d027c69361573
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-08-07 14:00:12 +0000

    maybe this one

--- musa/custom_storage.py
+++ musa/custom_storage.py
@@ -1,13 +1,23 @@
 from cloudinary_storage.storage import StaticHashedCloudinaryStorage
+from cloudinary.api import resources_by_tag
 
 
 class CustomCloudinaryStorage(StaticHashedCloudinaryStorage):
 
     def _exists_with_etag(self, name, content):
         """
-        Override this method to implement custom logic
-        or simply return False to bypass the check.
+        Override this method to implement custom logic.
         """
-        # For demonstration purposes, we're simply bypassing the check.
-        # You can implement custom logic here if needed.
+        # Use Cloudinary's API to check if a file with the given name exists.
+        # This example uses the resources_by_tag method, assuming that 'name' can be used as a tag.
+        # Modify the logic based on your needs and Cloudinary's available APIs.
+        try:
+            resources = resources_by_tag(name)
+            if resources and resources['resources']:
+                return True
+        except Exception as e:
+            # Handle any exceptions that arise from the API call
+            print(f"Error checking existence of file {name}: {e}")
+
+        # If no resources are found with the given name, return False.
         return False
