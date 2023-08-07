48cfb3546bdf6c13041b3491a361f0fe29d44bf8
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-08-07 08:03:35 +0000

    fix: attempt to solve cloudinary issue

--- django_musa/settings.py
+++ django_musa/settings.py
@@ -174,6 +174,11 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
